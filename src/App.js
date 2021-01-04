0d05f01a5d0f96d883bdcb6b08702acc51d754e5
강상원 <kevink1113@u.sogang.ac.kr>
2021-01-04 21:56:22 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import "./App.css";
+import Weather from "./Weather";
+import axios from "axios";
+
+class App extends React.Component {
+    state = {
+        isLoading: true,
+        id: "",
+        img: "",
+        description: "",
+        temp: "",
+    };
+
+    getWeathers() {
+        axios
+            .get(
+                "http://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=3ff45f341f358a1d2fa7afc352c35dac&units=metric"
+            )
+            .then((response) => {
+                console.log(response);
+                const data = response.data;
+                console.log(data);
+                const weather = data.weather[0];
+
+                this.setState({
+                    id: weather.id,
+                    img: weather.icon,
+                    isLoading: false,
+                    description: weather.description,
+                    temp: data.main.temp,
+                });
+            });
+    }
+
+    componentDidMount() {
+        this.getWeathers();
+    }
+
+    render() {
+        const { isLoading, img, description, temp } = this.state;
+        // console.log(weathers);
+        const imgUrl = `http://openweathermap.com/img/w/${img}.png`;
+
+        return (
+            <section className="container">
+                {isLoading ? (
+                    <div className="loader">
+                        <span className="loader_text">Loading...</span>
+                    </div>
+                ) : (
+                    <div className="weathers">
+                        <Weather img={imgUrl} main={description} temp={temp} />
+                    </div>
+                )}
+            </section>
+        );
+    }
 }
 
 export default App;
---
+++ src/Weather.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import "./Weather.css";
+
+function Weather({ img, main, temp }) {
+    // console.log(id);
+    return (
+        <div className="weather">
+            <h1>Your current weather at Seoul is: </h1>
+            <img src={img} />
+            <h2>{main}</h2>
+            <h3>temp: {temp}°C</h3>
+        </div>
+    );
+}
+
+export default Weather;
